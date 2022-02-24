2451c6fcdefca0b533e88e9235dd4b3b855df449
Masashi Kawakami <m_kawakami@avelio.co.jp>
2022-02-24 20:34:10 +0900

    FEATURE/Navigation Bar/Main Page

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <NavigationBar/>
+    <MainPage/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import MainPage from '@/components/MainPage';
+import NavigationBar from '@/components/NavigationBar';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MainPage,
+    NavigationBar
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100vh;
 }
 </style>
